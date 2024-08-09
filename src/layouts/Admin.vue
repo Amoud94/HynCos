1be1a894d3f206ecb459a89360c74df876e15d7d
user <user@192.168.1.7>
2024-08-09 01:32:23 +0100

    first commit

---
+++ src/layouts/Admin.vue
@@ -0,0 +1,16 @@
+<template>
+    
+    <div class="w-full flex flex-col h-screen items-center justify-center bg-[#03A9F4] p-10" > 
+       <RouterView  />
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
